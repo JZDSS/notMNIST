cbddc795a45a636431d865a6acc6a27f957cb319
QiYao <qy@stu.scu.edu.cn>
2017-02-26 18:19:48 +0800

    tensorboard
    Signed-off-by: QiYao <qy@stu.scu.edu.cn>

--- notMNIST.py
+++ notMNIST.py
@@ -12,8 +12,8 @@ CHANNELS = 1
 BATCH_SIZE = 64
 NUM_CLASSES = 10
 PIXEL_DEPTH = 255
-
-
+LOGDIR = './LOG'
+sess = tf.Session()
 def get_data_and_labels():
 	if not os.path.exists('train_data.pkl'):
 		letter_list = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
@@ -79,7 +79,9 @@ X = tf.placeholder(tf.float32, [None, IMAGE_SIZE, IMAGE_SIZE, CHANNELS])
 conv1_weights = tf.Variable(tf.truncated_normal(
 	[5, 5, CHANNELS, 64],  # 5x5 filter, depth 32.
 	stddev=0.1))
+tf.summary.histogram("conv1_w", conv1_weights)
 conv1_biases = tf.Variable(tf.zeros([64]))
+tf.summary.histogram('conv1_b', conv1_biases)
 conv2_weights = tf.Variable(tf.truncated_normal(
 	[5, 5, 64, 128], stddev=0.1))
 conv2_biases = tf.Variable(tf.constant(0.1, shape=[128]))
@@ -99,11 +101,11 @@ def model(data, train=False):
 						strides=[1, 1, 1, 1],
 						padding='SAME')
 	relu = tf.nn.relu(tf.nn.bias_add(conv, conv1_biases))
-	
 	pool = tf.nn.max_pool(relu,
 						  ksize=[1, 2, 2, 1],
 						  strides=[1, 2, 2, 1],
 						  padding='SAME')
+	
 	conv = tf.nn.conv2d(pool,
 						conv2_weights,
 						strides=[1, 1, 1, 1],
@@ -131,18 +133,27 @@ Y_ = tf.placeholder(tf.int64, [None, ])
 loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(logits=Y, labels=Y_))
 
 cross_entropy = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(logits=P, labels=Y_))
+tf.summary.scalar('xent', cross_entropy)
+
 is_correct = tf.equal(tf.argmax(tf.nn.softmax(P), 1), Y_)
 accuracy = tf.reduce_mean(tf.cast(is_correct, tf.float32))
+tf.summary.scalar('acc', accuracy)
+
+
 batch = tf.Variable(0, dtype=tf.float32)
 learning_rate = tf.train.exponential_decay(0.01, batch * BATCH_SIZE, train_labels.shape[0], 0.95, staircase=True)
-
+# learning_rate = tf.train.exponential_decay(0.01, batch * BATCH_SIZE, 1, 0.95, staircase=True)
+tf.summary.scalar('lr', learning_rate)
 optimizer = tf.train.GradientDescentOptimizer(learning_rate)
 train_step = optimizer.minimize(loss)
 
 init = tf.global_variables_initializer()
-sess = tf.Session()
-sess.run(init)
 
+sess.run(init)
+summ = tf.summary.merge_all()
+saver = tf.train.Saver()
+writer = tf.summary.FileWriter(LOGDIR)
+writer.add_graph(sess.graph)
 
 def get_batch(data, labels):
 	id = np.random.randint(low=0, high=train_labels.shape[0], size=BATCH_SIZE, dtype=np.int32)
@@ -157,8 +168,10 @@ for i in range(100000):
 	train_feed = {X: batch_X, Y_: batch_Y}
 	sess.run(train_step, feed_dict=train_feed)
 	# print(sess.run(conv1_weights))
-	a, c = sess.run([accuracy, cross_entropy], feed_dict=train_feed)
-	print('step %04d batch acc: %02d%%, ce: %02.02f' % (i, a * 100, c))
+	a, c, s = sess.run([accuracy, cross_entropy, summ], feed_dict=train_feed)
+	writer.add_summary(s, i)
+	print('step %06d batch acc: %02d%%, ce: %02.02f' % (i, a * 100, c))
+	saver.save(sess, os.path.join(LOGDIR, 'model.ckpt'), i)
 	if ((i - 99) % 100) == 0:
 		valid_feed = {X: valid_data, Y_: valid_labels}
 		a, c = sess.run([accuracy, cross_entropy], feed_dict=valid_feed)
